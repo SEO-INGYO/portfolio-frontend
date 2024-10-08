30ed26133cc49430404af7b240197a67d11e7527
서인교 <saingo4202@gmail.com>
2024-10-08 16:17:30 +0900

    애니메이션 추가

--- app/page.js
+++ app/page.js
@@ -1,25 +1,33 @@
-import React from 'react';
+"use client";
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import About from '@/components/About';
 import Skills from '../components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div>
-      <section id="about">
+      <section id="about" data-aos="fade-up">
         <About />
       </section>
       
-      <section id="skills">
+      <section id="skills" data-aos="fade-right">
         <Skills />
       </section>
 
-      <section id="projects">
+      <section id="projects" data-aos="fade-left">
         <Projects />
       </section>
       
-      <section id="contact">
+      <section id="contact" data-aos="zoom-in">
         <Contact />
       </section>
     </div>
